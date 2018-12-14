36681c521118619ce049c56860ca893a3a15aa88
airborne <qweasdz76@live.com>
2018-12-14 19:37:51 +0800

    first

---
+++ WebsiteForAsa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebsiteForAsa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
